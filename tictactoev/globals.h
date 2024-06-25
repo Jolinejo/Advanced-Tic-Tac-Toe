3397a9b39625712bc141bc5f7cc8552bca231c18
ViviannEmad <Vivianne.Emad@gmail.com>
2024-06-25 15:23:25 +0300

    added new music

--- tictactoev/globals.h
+++ tictactoev/globals.h
@@ -2,8 +2,13 @@
 #define GLOBALS_H
 
 #include "mainwindow.h"
+#include <QMediaPlayer>
+#include <QAudioOutput>
 
 extern User *player1;
 extern User *player2;
+
+inline QMediaPlayer *musicPlayer = new QMediaPlayer;
+inline QAudioOutput *audioOutput = new QAudioOutput;
 
 #endif  // GLOBALS_H
--- tictactoev/mainwindow.cpp
+++ tictactoev/mainwindow.cpp
@@ -1,20 +1,26 @@
 #include "mainwindow.h"
 #include "globals.h"
 #include "ui_mainwindow.h"
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent), ui(new Ui::MainWindow) {
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    musicPlayer->setSource(QUrl("qrc:/sounds/Sneaky.mp3"));
+    musicPlayer->setAudioOutput(audioOutput);
+    audioOutput->setVolume(0.5);
+    musicPlayer->play();
 }
 
 MainWindow::~MainWindow() {
     delete sign_;
     delete ui;
+    delete musicPlayer;
+    delete audioOutput;
 }
 
 void MainWindow::on_pushButton_playerxplayer_clicked() {
     hide();
     sign_ = new signin(this, "1");
     sign_->show();
+
 }
 
 void MainWindow::on_pushButton_AI_clicked() {
